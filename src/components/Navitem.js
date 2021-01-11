814f0fffa010d928d31c0c49aba1ec51057818d6
akazachek <alexdkazachek@gmail.com>
2021-01-11 15:13:48 -0500

    Fixed the navbar selection I accidentally deleted

--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -6,7 +6,7 @@ class NavItem extends Component {
     render() {
         return (
             <li id = {this.props.item} class = "hvr-sweep-to-right">
-                <Link to = {this.props.tolink}>{this.props.item}</Link>
+                <Link to = {this.props.tolink} onClick = {this.props.click.bind(this, this.props.item)}>{this.props.item}</Link>
             </li>
         )
     }
